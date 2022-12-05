0f45755f9157e60a277d5756d91b93f612b9a086
imevasoldi <imevasoldi@gmail.com>
2022-12-05 13:15:05 -0600

    main content css

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,13 @@
 function Main () {
-    return <h2>main content goes here</h2>
+    return <main className="main--title">
+        <h1>Fun Facts About React</h1>
+        <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powersthousands of enterprise apps, including mobile apps</li>
+        </ul> 
+        </main>
 }
 export default Main
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
-function Navbar () {
-    return <nav>
-        <img src="../react.svg"/>
-        <h3>ReactFacts</h3>
-        <h4>React Course - Project 1</h4>
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../react.svg" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
+    )
 }
-export default Navbar
