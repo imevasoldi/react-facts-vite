3459fde64b8a7866f7cac3130457da881de778a1
imevasoldi <imevasoldi@gmail.com>
2022-12-05 09:55:34 -0600

    nav css

---
+++ src/Components/Main.jsx
@@ -0,0 +1,4 @@
+function Main () {
+    return <h2>main content goes here</h2>
+}
+export default Main
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar () {
+    return <nav>
+        <img src="../react.svg"/>
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+        </nav>
+}
+export default Navbar
