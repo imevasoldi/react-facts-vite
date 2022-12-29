5ef4a1409a0baf832e62cef1cca8823fa38d0e04
imevasoldi <imevasoldi@gmail.com>
2022-12-29 13:42:40 -0600

    re-checking project css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import reactLogo from '../public/vite.svg'
 import './App.css'
 import Main from './Components/Main'
 import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
